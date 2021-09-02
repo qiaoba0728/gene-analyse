f45a42d039e028f38b0a746bbd186ba1a1963d14
hexing <hexing@yunify.com>
2021-09-02 22:46:05 +0800

    test

--- cmd/test.go
+++ cmd/test.go
@@ -1,16 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/qiaoba0728/gene-analyse/internal/types"
-	"github.com/qiaoba0728/gene-analyse/internal/utils"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strings"
-	"time"
 )
 
 var testCmd = &cobra.Command{
@@ -21,7 +12,7 @@ var testCmd = &cobra.Command{
 		//conf.InitConfig(cfgFile)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		g, _ := utils.NewZapLogger(&utils.Opt{LogOutput: utils.CONSOLE})
+		//g, _ := utils.NewZapLogger(&utils.Opt{LogOutput: utils.CONSOLE})
 		//plugin := build.NewReportPlugin(l)
 		//if err := plugin.Build(context.Background()); err != nil {
 		//	l.Error("report  fail", zap.Error(err))
@@ -46,59 +37,59 @@ var testCmd = &cobra.Command{
 		//	fmt.Println("cmd", command.String())
 		//	return
 		//}
-		vcfs := ""
-		files, err := ioutil.ReadDir(types.GATK_OUT)
-		if err != nil {
-			return
-		}
-		for _, v := range files {
-			if strings.HasSuffix(v.Name(), ".g.vcf") {
-				//vcfs = append(vcfs,fmt.Sprintf("%s/%s",types.GATK_OUT,v.Name()))
-				vcfs = vcfs + fmt.Sprintf("-V %s/%s ", types.GATK_OUT, v.Name())
-			}
-		}
-		if vcfs != "" {
-			start := time.Now()
-			temp := "merge"
-			start = time.Now()
-			vcfs = "gatk CombineGVCFs -R gene.fa " + vcfs + " -O " + fmt.Sprintf("%s.g.vcf", temp)
-			cmd := exec.Command("/bin/sh", "-c", vcfs)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err = cmd.Run(); err != nil {
-				g.Error("run gatk CombineGVCFs bam", zap.Error(err), zap.String("cmd", cmd.String()))
-				return
-			}
-			g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
-			start = time.Now()
-			cmd = exec.Command("gatk", "GenotypeGVCFs", "-R", "gene.fa",
-				"-V", fmt.Sprintf("%s/%s.g.vcf", types.GATK_OUT, temp),
-				"-O", fmt.Sprintf("%s/%s.vcf", types.GATK_OUT, temp))
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err = cmd.Run(); err != nil {
-				g.Error("run gatk GenotypeGVCFs bam", zap.Error(err), zap.String("cmd", cmd.String()))
-				return
-			}
-
-			cmd = exec.Command("bgzip", "-f", fmt.Sprintf("%s/%s.vcf", types.GATK_OUT, temp))
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err = cmd.Run(); err != nil {
-				g.Error("run bgzip bam", zap.Error(err), zap.String("cmd", cmd.String()))
-				return
-			}
-			g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
-			//tabix -p vcf ${file}.vcf.gz
-			start = time.Now()
-			cmd = exec.Command("tabix", "-p", "vcf", fmt.Sprintf("%s/%s.vcf.gz", types.GATK_OUT, temp))
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err = cmd.Run(); err != nil {
-				g.Error("run tabix bam", zap.Error(err), zap.String("cmd", cmd.String()))
-				return
-			}
-			g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
-		}
+		//vcfs := ""
+		//files, err := ioutil.ReadDir(types.GATK_OUT)
+		//if err != nil {
+		//	return
+		//}
+		//for _, v := range files {
+		//	if strings.HasSuffix(v.Name(), ".g.vcf") {
+		//		//vcfs = append(vcfs,fmt.Sprintf("%s/%s",types.GATK_OUT,v.Name()))
+		//		vcfs = vcfs + fmt.Sprintf("-V %s/%s ", types.GATK_OUT, v.Name())
+		//	}
+		//}
+		//if vcfs != "" {
+		//	start := time.Now()
+		//	temp := "merge"
+		//	start = time.Now()
+		//	vcfs = "gatk CombineGVCFs -R gene.fa " + vcfs + " -O " + fmt.Sprintf("%s.g.vcf", temp)
+		//	cmd := exec.Command("/bin/sh", "-c", vcfs)
+		//	cmd.Stdout = os.Stdout
+		//	cmd.Stderr = os.Stderr
+		//	if err = cmd.Run(); err != nil {
+		//		g.Error("run gatk CombineGVCFs bam", zap.Error(err), zap.String("cmd", cmd.String()))
+		//		return
+		//	}
+		//	g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
+		//	start = time.Now()
+		//	cmd = exec.Command("gatk", "GenotypeGVCFs", "-R", "gene.fa",
+		//		"-V", fmt.Sprintf("%s/%s.g.vcf", types.GATK_OUT, temp),
+		//		"-O", fmt.Sprintf("%s/%s.vcf", types.GATK_OUT, temp))
+		//	cmd.Stdout = os.Stdout
+		//	cmd.Stderr = os.Stderr
+		//	if err = cmd.Run(); err != nil {
+		//		g.Error("run gatk GenotypeGVCFs bam", zap.Error(err), zap.String("cmd", cmd.String()))
+		//		return
+		//	}
+		//
+		//	cmd = exec.Command("bgzip", "-f", fmt.Sprintf("%s/%s.vcf", types.GATK_OUT, temp))
+		//	cmd.Stdout = os.Stdout
+		//	cmd.Stderr = os.Stderr
+		//	if err = cmd.Run(); err != nil {
+		//		g.Error("run bgzip bam", zap.Error(err), zap.String("cmd", cmd.String()))
+		//		return
+		//	}
+		//	g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
+		//	//tabix -p vcf ${file}.vcf.gz
+		//	start = time.Now()
+		//	cmd = exec.Command("tabix", "-p", "vcf", fmt.Sprintf("%s/%s.vcf.gz", types.GATK_OUT, temp))
+		//	cmd.Stdout = os.Stdout
+		//	cmd.Stderr = os.Stderr
+		//	if err = cmd.Run(); err != nil {
+		//		g.Error("run tabix bam", zap.Error(err), zap.String("cmd", cmd.String()))
+		//		return
+		//	}
+		//	g.Info("run cmd finished", zap.String("cmd", cmd.String()), zap.Duration("lost", time.Since(start)))
+		//}
 	},
 }
